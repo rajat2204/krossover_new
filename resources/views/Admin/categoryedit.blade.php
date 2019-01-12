e98ac352a0857440511b2dc5856b40ba4cbe60c1
mohdkaif <mohdkaif984@gmail.com>
2019-01-12 14:40:35 +0530

    changes

--- resources/views/Admin/categoryedit.blade.php
+++ resources/views/Admin/categoryedit.blade.php
@@ -1,10 +1,20 @@
+
 <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
-	<div class="row">
-		<div class="col-lg-12">
-			<h1 class="page-header">Edit Main Category</h1>
-		</div>
-	</div><!--/.row-->
-	<div class="panel panel-default">
+		<div class="row">
+			<ol class="breadcrumb">
+				<li><a href="#">
+					<em class="fa fa-home"></em>
+				</a></li>
+				<li class="active">Edit Categories</li>
+			</ol>
+		</div><!--/.row-->
+		
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">Edit Categories</h1>
+			</div>
+		</div><!--/.row-->
+		<div class="panel panel-default">
 		<div class="panel-body">
 			<div class="col-md-6">
 				<form role="edit-category" data-request="enable-enter" action="{{url('admin/categories/'.___encrypt($category['id']))}}" method="POST" class="form-horizontal form-label-left">
@@ -28,9 +38,8 @@
 				</form>
 			</div>
 		</div>
-	</div>
+	</div>		
 </div>
-
 <script type="text/javascript">
     setTimeout(function(){
         $('[data-request="enable-enter"]').on('keyup','input',function (e) {
