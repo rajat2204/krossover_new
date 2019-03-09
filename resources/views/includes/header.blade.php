dfdece104cdfe1509e4b938fa97a44637907654b
preetiverma123 <preetiverma123>
2019-03-09 09:47:58 +0530

    changes

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -36,7 +36,7 @@
               <li class="nav-item  @if(Request::segment(1)=='') active @endif"><a class="nav-link" href="{{url('/')}}">Home</a></li>
               <li class="nav-item @if(Request::segment(2)=='aboutus') active @endif"><a class="nav-link" href="{{url('pages/aboutus')}}">About Us</a></li>
               <li class="nav-item @if(Request::segment(1)=='whyus') active @endif"><a class="nav-link" href="{{url('whyus')}}">Why Us</a></li>
-              <li class="nav-item @if(Request::segment(1)=='gallery') active @endif"><a class="nav-link" href="{{url('gallery')}}">Inspirational Galleries</a></li>
+              <li class="nav-item @if(Request::segment(1)=='gallery') active @endif"><a class="nav-link" href="{{url('gallery')}}">Inspirational Gallery</a></li>
               <li class="nav-item @if(Request::segment(1)=='most-popular') active @endif"><a class="nav-link" href="{{url('most-popular')}}">Featured Products</a></li>
               <li class="nav-item @if(Request::segment(1)=='catalogue') active @endif"><a class="nav-link" href="{{url('/catalogue')}}">Catalogue</a></li>
               <li class="nav-item submenu dropdown @if(Request::segment(2)=='main') active @endif"><a class="nav-link" href="javascipt:void(0);">Products</a>
