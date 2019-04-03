20ee3db4db2423b50dd73637cb1ed2edb3aedfe9
preetiverma123 <preetiverma123>
2019-04-03 08:45:13 +0530

    changes

--- resources/views/front/view-category.blade.php
+++ resources/views/front/view-category.blade.php
@@ -22,9 +22,9 @@
             <div class="deal-details">
               <h6 class="deal-title">{{!empty($category['name'])?$category['name']:''}}</h6>
             </div>
-            <div class="deal-detailsshow">
+            <!-- <div class="deal-detailsshow">
                 <h6 class="deal-titleshow">{{str_limit($category['name'],19)}}</h6>
-            </div>
+            </div> -->
           </a>
         </div>
       @endforeach
